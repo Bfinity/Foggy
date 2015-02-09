31f5f6e759ff29529aa0e96d1bc6a671c02f8d1e
Brandy Foster <brandy.foster@detroitlabs.com>
2015-02-09 12:18:05 -0500

    added permission for boot completed to be received

---
+++ app/src/main/java/com/example/bfinerocks/foggy/FogCastReceiver.java
@@ -0,0 +1,15 @@
+package com.example.bfinerocks.foggy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by BFineRocks on 2/9/15.
+ */
+public class FogCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
